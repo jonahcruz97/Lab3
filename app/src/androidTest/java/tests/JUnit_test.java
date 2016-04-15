d0c9c97c943a9193a19d39390330d9bf44bc94de
Steven Lee <stl028@UCSD.EDU>
2016-04-15 14:52:05 -0700

    First try

---
+++ app/src/androidTest/java/tests/JUnit_test.java
@@ -0,0 +1,47 @@
+package tests;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.stl028.lab31.MainActivity;
+import com.example.stl028.lab31.R;
+
+/**
+ * Created by jtc025 on 4/13/2016.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    MainActivity myActivity;
+    public JUnit_test () {
+
+        super (MainActivity.class);
+
+    }
+
+    public void test_first () {
+
+        myActivity = getActivity();
+
+        TextView textView = (TextView) myActivity.findViewById(R.id.tv);
+        int tester = myActivity.sum(2, 2);
+        assertEquals(4, tester);
+
+        tester = myActivity.sum (0, 0);
+        assertEquals(0, tester);
+
+        tester = myActivity.sum(-1, -2);
+        assertEquals(-3, tester);
+
+        tester = myActivity.sum(1, -2);
+        assertEquals(-1, tester);
+
+        tester = myActivity.sum(-1, 2);
+        assertEquals(1, tester);
+
+        tester = myActivity.sum(1, -2);
+        assertEquals(-1, tester);
+
+        tester = myActivity.sum(1, -1);
+        assertEquals(0, tester);
+
+    }
+}
---
+++ app/src/main/java/com/example/stl028/lab31/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.stl028.lab31;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et;
+    EditText et2;
+    TextView result;
+    Button button;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    //private GoogleApiClient client;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        et = (EditText) findViewById(R.id.editText1);
+        et2 = (EditText) findViewById(R.id.editText2);
+        result = (TextView) findViewById(R.id.tv);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1 = Integer.parseInt(et.getText().toString());
+                int num2 = Integer.parseInt(et2.getText().toString());
+                int sum = sum(num1, num2);
+                result.setText(Integer.toString(sum));
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public int sum(int num1, int num2) {
+
+        return num1 + num2;
+    }
+}
